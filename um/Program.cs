1d17d97b6d5188425c13464c006ee172f225c139
vinibenfatti <vinibenfatti@gmail.com>
2021-02-03 14:15:50 -0300

    Adicionar arquivos de projeto.

---
+++ um/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace um
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] km = Console.ReadLine().Split(" ");
+
+            int horas = Int32.Parse(km[0]);
+            int velocidade = Int32.Parse(km[1]);
+
+            double autonomia= 12;
+            double litro = (horas * velocidade);
+            double total = litro / autonomia;
+
+            Console.WriteLine(total.ToString("N3"));
+
+        }
+    }
+}
